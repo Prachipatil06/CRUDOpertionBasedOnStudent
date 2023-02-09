c84ccee0725af3cb0bb3f64bfceaa6ba38cd213b
Prachipatil06 <124551935+Prachipatil06@users.noreply.github.com>
2023-02-09 19:51:51 +0530

    Update B.java

--- src/com/techpalle/B.java
+++ src/com/techpalle/B.java
@@ -2,10 +2,11 @@ package com.techpalle;
 
 public class B
 {
-  public int i;
-  
-  public B(int i)
+ public int i;
+ public int j;
+ public int k; 
+ public B(int i)
   {
-	  this.i=i;
+	 this.i=i;
   }
 }
